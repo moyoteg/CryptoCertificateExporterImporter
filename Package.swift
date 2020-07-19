a8c2eb83cded248175a3f203cd8dfb19590b344e
Moi Gutierrez <moyoteg@gmail.com>
2020-07-18 19:10:39 -0700

    fixed names

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "CryptoKeyExporterAndImporter",
+    name: "CryptoCertificateExporterImporter",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CryptoKeyExporterAndImporter",
-            targets: ["CryptoKeyExporterAndImporter"]),
+            name: "CryptoCertificateExporterImporter",
+            targets: ["CryptoCertificateExporterImporter"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CryptoKeyExporterAndImporter",
+            name: "CryptoCertificateExporterImporter",
             dependencies: []),
         .testTarget(
-            name: "CryptoKeyExporterAndImporterTests",
-            dependencies: ["CryptoKeyExporterAndImporter"]),
+            name: "CryptoCertificateExporterImporterTests",
+            dependencies: ["CryptoCertificateExporterImporter"]),
     ]
 )
