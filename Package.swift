14e6041930d458f1855c089ffce20d05e09181f9
Moi Gutierrez <moyoteg@gmail.com>
2020-07-18 19:02:30 -0700

    fixed package

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "CryptoKeyExporterAndImporterTests",
-            dependencies: ["CryptoKeyExporterAndImporterManager"]),
+            dependencies: ["CryptoKeyExporterAndImporter"]),
     ]
 )
